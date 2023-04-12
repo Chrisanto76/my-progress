fe111ecb0c45a0c8c81d27dc42c2eb1c3164bf11
Crhisanto76 <antonypoure2@gmail.com>
2023-04-12 15:30:50 +0200

    add css folder and component folder

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+export default  function menu() {
+    return (
+        <div className="menu">
+            <ul>
+                <li>Home</li>
+                <li>Add Techno</li>
+                <li>All Technos</li>
+            </ul>
+        </div>
+    )
+}
